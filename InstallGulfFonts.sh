d71138f9c4974d459a61bea437de838b58736148
Nirawat Prasertsung <nirawat.pss@gmail.com>
2022-02-18 12:17:28 +0700

    commit

--- InstallGulfFonts.sh
+++ InstallGulfFonts.sh
@@ -11,27 +11,21 @@ echo "*** NOTICE: Install apt-get install"
 apt-get install
 apt-get update
 apt-get install fontconfig
+apt-get install - reinstall ttf-mscorefonts-installer
 apt-get -y install git
 echo ""
 echo "*** NOTICE: We've use root [sudo] mode. please enter the password. ***"
 echo "Downloading..."
 cd ..
 cd usr/share/fonts/truetype
-apt-get install - reinstall ttf-mscorefonts-installer
-wget -q -O - https://gist.githubusercontent.com/Blastoise/72e10b8af5ca359772ee64b6dba33c91/raw/2d7ab3caa27faa61beca9fbf7d3aca6ce9a25916/clearType.sh | bash
-mkdir thai-sarabun-new
-git clone https://github.com/inwdragon/thsn-for-ubuntu.git
+mkdir gulf-fonts
+git clone https://github.com/nirawat/install_gulf_font.git
 echo "Download complete!"
 echo ""
 echo "Installing..."
-mkdir /usr/share/fonts/truetype/thai-sarabun-new
-mv `pwd`/thsn-for-ubuntu/*.ttf /usr/share/fonts/truetype/thai-sarabun-new/
+mv `pwd`/install_gulf_font/fonts/*.ttf ./gulf-fonts
 fc-cache -f -v
 fc-list
 echo "Install complete!"
-echo ""
-echo "Removing install package..."
-rm -rf `pwd`/thsn-for-ubuntu/
-echo "Remove install package complete!"
 echo ""
 echo "*** Install Font complete ***"
