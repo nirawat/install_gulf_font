b5faa31000346b120b265fc940ce3e630e4e5c68
Nirawat Prasertsung <nirawat.pss@gmail.com>
2022-02-21 10:17:06 +0700

    commit

--- InstallGulfFonts.sh
+++ InstallGulfFonts.sh
@@ -7,14 +7,6 @@ APP_VERSION="1.0"
 echo "# Install Font for Ubuntu terminal."
 echo "# Version $APP_VERSION"
 echo ""
-echo "*** NOTICE: We've use root [sudo] mode. please enter the password. ***"
-echo "Downloading Font..."
-cd ..
-cd usr/share/fonts/truetype
-mkdir gulf-fonts
-git clone https://github.com/nirawat/install_gulf_font.git
-echo "Download complete!"
-echo ""
 echo "*** Install apt-get & git"
 apt-get install
 apt-get update
@@ -22,7 +14,13 @@ apt install fontconfig
 apt install - reinstall ttf-mscorefonts-installer
 apt -y install git
 echo ""
-cd gulf-fonts
+echo "*** NOTICE: We've use root [sudo] mode. please enter the password. ***"
+echo "Downloading Font..."
+cd ..
+cd usr/share/fonts/truetype
+mkdir gulf-fonts
+git clone https://github.com/nirawat/install_gulf_font.git
+echo "Download complete!"
 echo ""
 echo "Config & Installing..."
 mv `pwd`/install_gulf_font/fonts/*.ttf ./gulf-fonts
