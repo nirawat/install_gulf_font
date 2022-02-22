b593e3c2592a6c2b83ab7a18b8646fa0b31ea307
Nirawat Prasertsung <nirawat.pss@gmail.com>
2022-02-22 11:27:24 +0700

    commit

--- InstallGulfFontGits.sh
+++ InstallGulfFontGits.sh
@@ -11,13 +11,13 @@ echo "*** NOTICE: Install apt-get install"
 apt-get install
 apt-get update
 apt-get install fontconfig
+apt-get install - reinstall ttf-mscorefonts-installer
 apt-get -y install git-all
 echo ""
 echo "*** NOTICE: We've use root [sudo] mode. please enter the password. ***"
 echo "Downloading..."
 cd ..
 cd usr/share/fonts/truetype
-apt-get install - reinstall ttf-mscorefonts-installer
 wget -q -O - https://gist.githubusercontent.com/Blastoise/72e10b8af5ca359772ee64b6dba33c91/raw/2d7ab3caa27faa61beca9fbf7d3aca6ce9a25916/clearType.sh | bash
 mkdir thai-sarabun-new
 git clone https://github.com/inwdragon/thsn-for-ubuntu.git
@@ -30,8 +30,8 @@ fc-cache -f -v
 fc-list | grep THSarabunNew
 echo "Install complete!"
 echo ""
-echo "Removing install package..."
-rm -rf `pwd`/thsn-for-ubuntu/
-echo "Remove install package complete!"
-echo ""
-echo "*** Install TH Sarabun New Font complete ***"
+echo "============= License =============="
+echo "By : Nirawat Prasertsang"
+echo "Email : nirawat.pss@gmail.com"
+echo "Mobile : 0886380638"
+echo "===================================="
